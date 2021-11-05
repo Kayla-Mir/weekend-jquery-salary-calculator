2ec07684d3762805bdc0a678f66a0f1202aa6535
Kayla Mir <kayla.mir32@gmail.com>
2021-11-05 18:46:56 -0500

    added displaying total monthly costs to the DOM and formatted the numbers with .toLocaleString

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let employees = [];
 
 function readyNow() {
     $('#add-employee-btn').on('click', handleAddEmployeeClick);
+    renderEmployees(employees);
 }
 
 function handleAddEmployeeClick() {
@@ -18,7 +19,7 @@ function handleAddEmployeeClick() {
         lastName: lastName,
         idNum: idNum,
         titleName: titleName,
-        annualSalary: annualSalary
+        annualSalary: Number(annualSalary)
     };
 
     employees.push(newEmployee);
@@ -27,5 +28,43 @@ function handleAddEmployeeClick() {
     $('#last-name-input').val('Mir');
     $('#id-input').val('1');
     $('#title-input').val('Programmer');
-    $('#annual-salary-input').val('25000');
+    $('#annual-salary-input').val(25000);
+
+    renderEmployees(employees);
+    renderTotalMonthlyOutput(employees);
+}
+
+function renderEmployees(peopleToRender) {
+    $('#employee-table-body').empty();
+
+    for (let employee of peopleToRender) {
+        let newTableRow = `
+            <tr>
+                <td>${employee.firstName}</td>
+                <td>${employee.lastName}</td>
+                <td>${employee.idNum}</td>
+                <td>${employee.titleName}</td>
+                <td>$${employee.annualSalary.toLocaleString('en-US')}</td>
+                <td><button id="remove-btn">Remove</button></td>
+            </tr>
+        `;
+        $('#employee-table-body').append(newTableRow);
+    }
+    
+}
+
+function renderTotalMonthlyOutput(itemsToSum) {
+    let totalSum = calculateTotalMonthlyOutput(itemsToSum);
+
+    $('#total-monthly-output').text(totalSum.toLocaleString('en-US', 
+        {maximumFractionDigits: 2}));
+}
+
+function calculateTotalMonthlyOutput(itemsToSum) {
+    let sum = 0;
+
+    for (let annualSalary of itemsToSum) {
+        sum += annualSalary.annualSalary / 12;
+    }
+    return sum;
 }
